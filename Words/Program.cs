2f3847dac6e968445d277f613e88a4658a61a3ef
zhuyst <13553371651@139.com>
2018-04-18 21:16:27 +0800

    四六级单词录入

---
+++ Words/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Words
+{
+    static class Program
+    {
+        static void Main()
+        {
+            var list = Words.GetWords();
+            foreach (var word in list)
+            {
+                Debug.WriteLine(word);
+            }
+        }
+    }
+}
---
+++ Words/Words.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Words
+{
+    public class Words
+    {
+        private const string FileName = "words";
+
+        private static List<string> _words;
+
+        public static List<string> GetWords()
+        {
+            if (_words != null)
+            {
+                return _words;
+            }
+
+            var str = ReadFile();
+            _words = str.Split(',').ToList();
+            return _words;
+        }
+
+        private static string ReadFile()
+        {
+            var assembly = Assembly.GetEntryAssembly();
+            var manager = new ResourceManager("Words.Properties.Resources", assembly);
+            return manager.GetString(FileName);
+        }
+    }
+}
