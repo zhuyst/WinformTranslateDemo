1ea4306ade43ffbed10f713bea379c0f70932479
zhuyst <13553371651@139.com>
2018-04-18 17:08:07 +0800

    优化youdao

--- YouDao/TranslateApi.cs
+++ YouDao/TranslateApi.cs
@@ -11,40 +11,50 @@ namespace YouDao
 {
     public class TranslateApi
     {
-        private static readonly string APP_KEY = "7517e3e1d7c42033";
+        private const string AppKey = "7517e3e1d7c42033";
 
-        private static readonly string APP_SECRET = "tl6RlC0bKr1VcTld7EMTtPQoVIOMOWfM";
+        private const string AppSecret = "tl6RlC0bKr1VcTld7EMTtPQoVIOMOWfM";
 
-        private static readonly string API_URL = "https://openapi.youdao.com/";
+        private const string ApiUrl = "https://openapi.youdao.com/";
 
-        private RestClient _client;
+        private readonly RestClient _client;
 
         public TranslateApi()
         {
-            _client = new RestClient(API_URL);
+            _client = new RestClient(ApiUrl);
         }
 
         public TranslateResult EnToZh(string q)
         {
-            var from = "en";
-            var to = "zh-CHS";
-            var salt = Guid.NewGuid().ToString();
+            const string from = "en";
+            const string to = "zh-CHS";
 
-            MD5 md5 = new MD5CryptoServiceProvider();
-            var md5Str = APP_KEY + q + salt + APP_SECRET;
-            byte[] output = md5.ComputeHash(Encoding.UTF8.GetBytes(md5Str));
-            var sign = BitConverter.ToString(output).Replace("-", "");
+            var salt = GetSalt();
+            var sign = GetSign(q, salt);
 
             var request = new RestRequest("api");
             request.AddParameter("q", q);
             request.AddParameter("from", from);
             request.AddParameter("to", to);
-            request.AddParameter("appKey", APP_KEY);
+            request.AddParameter("appKey", AppKey);
             request.AddParameter("salt", salt);
             request.AddParameter("sign", sign);
 
             var response = _client.Post<TranslateResult>(request);
             return response.Data;
+        }
+
+        private static string GetSalt()
+        {
+            return Guid.NewGuid().ToString();
+        }
+
+        private static string GetSign(string q, string salt)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            var md5Str = AppKey + q + salt + AppSecret;
+            var output = md5.ComputeHash(Encoding.UTF8.GetBytes(md5Str));
+            return BitConverter.ToString(output).Replace("-", "");
         }
     }
 }
