720e4335de9ddf13dfb6a1265033e13ebbb4a69e
youda guo <MCGYDACG@outlook.com>
2018-04-18 16:45:23 +0800

    test

--- WinformTranslateDemo/Program.cs
+++ WinformTranslateDemo/Program.cs
@@ -13,7 +13,7 @@ namespace WinformTranslateDemo
         /// </summary>
         [STAThread]
         static void Main()
-        {
+        {   Console.WriteLine("git");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
