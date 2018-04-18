42d4c379624e0df6b4e896e031c751457d9b0752
zhuyst <13553371651@139.com>
2018-04-18 21:45:38 +0800

    清除未使用的using

--- Words/Words.cs
+++ Words/Words.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Resources;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Words
 {
--- YouDao/Program.cs
+++ YouDao/Program.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Newtonsoft.Json;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
 
 namespace YouDao
 {
