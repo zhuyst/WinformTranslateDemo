80a5e14fbd968925353d7718e13fe4094f2252f4
SaigyoujiKonpaku <1414242145@qq.com>
2018-04-18 22:55:59 +0800

    数据

--- WinformTranslateDemo/MainForm.cs
+++ WinformTranslateDemo/MainForm.cs
@@ -16,5 +16,21 @@ namespace WinformTranslateDemo
         {
             InitializeComponent();
         }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < 10; i++)   //添加10行数据
+            {
+                ListViewItem lvi = new ListViewItem("12343");
+
+                wordListView.Items.Add(lvi);
+            }
+        }
     }
 }
