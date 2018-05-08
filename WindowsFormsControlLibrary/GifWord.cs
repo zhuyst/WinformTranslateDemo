9beff89ab203d2b82fe795ce15242b150637748a
barry1101 <q369140975@outlook.com>
2018-05-08 21:03:01 +0800

    做了一些微小的工作

--- WindowsFormsControlLibrary/GifWord.cs
+++ WindowsFormsControlLibrary/GifWord.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Drawing;
+﻿using System.Drawing;
 using System.Windows.Forms;
 
 namespace WindowsFormsControlLibrary
@@ -19,29 +18,18 @@ namespace WindowsFormsControlLibrary
             var g = e.Graphics;
             if (string.IsNullOrEmpty(Word)) return;
             var font = new Font("黑体", 20);
-            Brush[] brush = { Brushes.Black, Brushes.Red, Brushes.Blue, Brushes.Green, Brushes.Brown, Brushes.Gold,Brushes.Pink };
             var str = g.MeasureString(Word, font);
             var strW = str.Width / Word.Length;
-            var brushe = Brushes.Black;
             //格式文本
             var sf = new StringFormat
             {
                 Alignment = StringAlignment.Near,   //alignment属性垂直
                 LineAlignment = StringAlignment.Center  //水平
             };
-            for (int i = 0; i < Word.Length; i++)
-            {   
-                //if (Frames > brush.Length - 1) Frames =-1;
-                if(i==Frames)
-                {                  
-                    brushe = Brushes.Red;
-                }
-                else
-                {
-                    brushe = Brushes.Black;
-                }
-                Console.WriteLine(i);
-                g.DrawString(Word[i].ToString(), font,brushe, i * strW, ClientSize.Height / 2, sf);
+            for (var i = 0; i < Word.Length; i++)
+            {
+                var brushe = i == Frames ? Brushes.Red : Brushes.Black;
+                g.DrawString(Word[i].ToString(), font, brushe, i * strW, ClientSize.Height / 2, sf);
             }
         }
     }
